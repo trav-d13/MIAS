4901f24c41d01afe86dcac322693347c29adc18f
trav-d13 <travisdawson64@gmail.com>
2023-10-12 20:51:17 +0200

    Streamlit initialized and started

---
+++ src/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("MIAS")
+st.markdown(
+    "MIAS is the **M**usically **I**lliterate **A**id **S**ystem designed to help developing artists expand on the "
+    "songs"
+    "they perform, by providing recommendations based on their current performance choices. I have very little "
+    "musical talent,"
+    ", but stil want to help, hence the name. ")
