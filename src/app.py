ca6beab7561f4d0c58bd421d7345c39303d86cd5
trav-d13 <travisdawson64@gmail.com>
2023-10-13 18:54:58 +0200

    Messing around with Streamlit functions and capabilities to understand them

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 st.title("MIAS")
 st.markdown(
@@ -7,3 +10,9 @@ st.markdown(
     "they perform, by providing recommendations based on their current performance choices. I have very little "
     "musical talent,"
     ", but stil want to help, hence the name. ")
+
+uploaded_file = st.file_uploader('Upload your file here')
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.describe())
