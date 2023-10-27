ff1e92333c9b5ed4e90ff7523e3ea1ad73e8011e
trav-d13 <travisdawson64@gmail.com>
2023-10-27 19:10:53 +0200

    Start of dataset info page and monitoring script

---
+++ src/pages/dataset.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Spotify Dataset")
+st.markdown("The below information details the collected Spotify track dataset.")
+st.markdown("This dataset is updated bi-weekly with the latest playlists and is freely available to download.")
