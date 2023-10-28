27dcc9b595029b412971724a81639a80cc7025f9
trav-d13 <travisdawson64@gmail.com>
2023-10-28 09:59:19 +0200

    Dataset growth plotted

--- src/pages/dataset.py
+++ src/pages/dataset.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import os
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 
 class Monitor:
@@ -35,3 +37,19 @@ end_date = st.slider(label='Select end date',
 
 if start_date >= end_date:
     st.write("Please make sure that start date comes before end date")
+else:
+    df = st.session_state.monitor.history
+    history_filtered = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
+
+    sns.set()
+    fig, axes = plt.subplots(1, 1, figsize=(12, 6))
+    h = sns.lineplot(data=history_filtered, x='date', y='track_count', color='black')
+    axes.fill_between(history_filtered['date'], history_filtered['track_count'], alpha=0.2, color='red')
+
+    axes.set_xlabel('Date')
+    axes.set_ylabel('Track Count')
+
+    st.pyplot(fig)
+
+
+st.header('Dataset Information')
