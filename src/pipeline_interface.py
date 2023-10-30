f63ff261efae43fb09928359748ee4dbaf501a60
trav-d13 <travisdawson64@gmail.com>
2023-10-30 14:19:30 +0200

    Pipeline interface documented

--- src/pipeline_interface.py
+++ src/pipeline_interface.py
@@ -10,6 +10,9 @@ class Pipeline(ABC):
     @abstractmethod
     def data_pipeline(df):
         """This method enacts the transformation pipeline to produce a set of track features.
-        It is expected for the playlist
+        It is expected for the playlist to return a pandas DataFrame containing all features.
+
+        Args:
+            df (DataFrame): The dataframe containing the raw data from the `data/tracks.csv` file
         """
         pass
